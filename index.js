60988e09a8eceb7cf39407b443a367a29e19e817
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2022-11-11 23:17:23 +0300

    Socket io added

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const socketio = require("socket.io");
 const express = require("express");
-
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("<h1>P2P Chat Backend</h1>");
 });
+io.on('connection', (socket) => {
+  console.log("User connected");
+  socket.broadcast.emit('online');
+  socket.on("chatMessage", (data) => {
+    socket.broadcast.emit('chatMessage', data);
+    console.log(data);
+  });
+  socket.on('disconnect', ()=>{
+    console.log('User disconnected')
+  })
+});
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Listening on port http://localhost:3000");
 });
